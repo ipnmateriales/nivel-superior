b12bc85e91994c5a55162779e36edfd05f7e53ba
ipnmateriales <ipnmaterialesns@gmail.com>
2026-02-11 23:51:13 -0600

    Add files via upload

---
+++ animaciones_de_la_pagina.js
@@ -0,0 +1,180 @@
+       const usuariosValidos = [
+            { username: "26mda0001", password: "ESub390)3", nombre: "Usuario Beta" },
+            { username: "26mda0002", password: "ESub635^8", nombre: "Usuario Beta" },
+            { username: "26mda0003", password: "ESub129@3", nombre: "Usuario Beta" },
+            { username: "26mda0004", password: "ESub872}8", nombre: "Usuario Beta" },
+            { username: "26mda0005", password: "ESub673<8", nombre: "Usuario Beta" },
+            { username: "26mda0006", password: "ESub574@0", nombre: "Usuario Beta" },
+            { username: "26mda0007", password: "ESub910}3", nombre: "Usuario Beta" },
+            { username: "26mda0008", password: "ESub217%5", nombre: "Usuario Beta" },
+            { username: "26mda0009", password: "ESub242+9", nombre: "Usuario Beta" },
+            { username: "26mda0010", password: "ESub251}9", nombre: "Usuario Beta" },
+            { username: "26mda0011", password: "ESub568]3", nombre: "Usuario Beta" },
+            { username: "26mda0012", password: "ESub463@8", nombre: "Usuario Beta" },
+            { username: "26mda0013", password: "ESub202_9", nombre: "Usuario Beta" },
+            { username: "26mda0014", password: "ESub837&0", nombre: "Usuario Beta" },
+            { username: "26mda0015", password: "ESub127{9", nombre: "Usuario Beta" },
+            { username: "26mda0016", password: "ESub701$1", nombre: "Usuario Beta" },
+            { username: "26mda0017", password: "ESub346>4", nombre: "Usuario Beta" },
+            { username: "26mda0018", password: "ESub697%4", nombre: "Usuario Beta" },
+            { username: "26mda0019", password: "ESub932}0", nombre: "Usuario Beta" },
+            { username: "26mda0020", password: "ESub733(7", nombre: "Usuario Beta" },
+            { username: "26mda0021", password: "ESub930}9", nombre: "Usuario Beta" },
+            { username: "26mda0022", password: "ESub565+7", nombre: "Usuario Beta" },
+            { username: "26mda0023", password: "ESub836?4", nombre: "Usuario Beta" },
+            { username: "26mda0024", password: "ESub983{0", nombre: "Usuario Beta" },
+            { username: "26mda0025", password: "ESub458(9", nombre: "Usuario Beta" },
+            { username: "26mda0026", password: "ESub509!6", nombre: "Usuario Beta" },
+            { username: "26mda0027", password: "ESub209$0", nombre: "Usuario Beta" },
+            { username: "26mda0028", password: "ESub389]1", nombre: "Usuario Beta" },
+            { username: "26mda0029", password: "ESub232?2", nombre: "Usuario Beta" },
+            { username: "26mda0030", password: "ESub993>3", nombre: "Usuario Beta" },
+            { username: "26mda0031", password: "ESub172)0", nombre: "Usuario Beta" },
+            { username: "26mda0032", password: "ESub374<1", nombre: "Usuario Beta" },
+            { username: "26mda0033", password: "ESub798}2", nombre: "Usuario Beta" },
+            { username: "26mda0034", password: "ESub852@2", nombre: "Usuario Beta" },
+            { username: "26mda0035", password: "ESub709^8", nombre: "Usuario Beta" },
+            { username: "26mda0036", password: "ESub285]8", nombre: "Usuario Beta" },
+            { username: "26mda0037", password: "ESub949*3", nombre: "Usuario Beta" },
+            { username: "26mda0038", password: "ESub688_8", nombre: "Usuario Beta" },
+            { username: "26mda0039", password: "ESub557[6", nombre: "Usuario Beta" },
+            { username: "26mda0040", password: "ESub959{6", nombre: "Usuario Beta" },
+            { username: "26mda0041", password: "ESub481)5", nombre: "Usuario Beta" },
+            { username: "26mda0042", password: "ESub850$2", nombre: "Usuario Beta" },
+            { username: "26mda0043", password: "ESub348_5", nombre: "Usuario Beta" },
+            { username: "26mda0044", password: "ESub638&6", nombre: "Usuario Beta" },
+            { username: "26mda0045", password: "ESub466@4", nombre: "Usuario Beta" },
+            { username: "26mda0046", password: "ESub765<4", nombre: "Usuario Beta" },
+            { username: "26mda0047", password: "ESub402_5", nombre: "Usuario Beta" },
+            { username: "26mda0048", password: "ESub474+5", nombre: "Usuario Beta" },
+            { username: "26mda0049", password: "ESub152<1", nombre: "Usuario Beta" },
+            { username: "26mda0050", password: "ESub898{0", nombre: "Usuario Beta" },
+            { username: "26mda0051", password: "ESub996#9", nombre: "Usuario Beta" },
+            { username: "26mda0052", password: "ESub504!7", nombre: "Usuario Beta" },
+            { username: "26mda0053", password: "ESub487#8", nombre: "Usuario Beta" },
+            { username: "26mda0054", password: "ESub621@9", nombre: "Usuario Beta" },
+            { username: "26mda0055", password: "ESub161*4", nombre: "Usuario Beta" },
+            { username: "26mda0056", password: "ESub356@8", nombre: "Usuario Beta" },
+            { username: "26mda0057", password: "ESub783{7", nombre: "Usuario Beta" },
+            { username: "26mda0058", password: "ESub180[1", nombre: "Usuario Beta" },
+            { username: "26mda0059", password: "ESub292*2", nombre: "Usuario Beta" },
+            { username: "26mda0060", password: "ESub410{6", nombre: "Usuario Beta" },
+            { username: "26mda0061", password: "ESub462_9", nombre: "Usuario Beta" },
+            { username: "26mda0062", password: "ESub603@4", nombre: "Usuario Beta" },
+            { username: "26mda0063", password: "ESub599(3", nombre: "Usuario Beta" },
+            { username: "26mda0064", password: "ESub861]7", nombre: "Usuario Beta" },
+            { username: "26mda0065", password: "ESub676+5", nombre: "Usuario Beta" },
+            { username: "26mda0066", password: "ESub780?6", nombre: "Usuario Beta" },
+            { username: "26mda0067", password: "ESub976&8", nombre: "Usuario Beta" },
+            { username: "26mda0068", password: "ESub840%7", nombre: "Usuario Beta" },
+            { username: "26mda0069", password: "ESub952?7", nombre: "Usuario Beta" },
+            { username: "26mda0070", password: "ESub225[8", nombre: "Usuario Beta" },
+            { username: "26mda0071", password: "ESub842)3", nombre: "Usuario Beta" },
+            { username: "26mda0072", password: "ESub502<2", nombre: "Usuario Beta" },
+            { username: "26mda0073", password: "ESub587%7", nombre: "Usuario Beta" },
+            { username: "26mda0074", password: "ESub130<5", nombre: "Usuario Beta" },
+            { username: "26mda0075", password: "ESub722}8", nombre: "Usuario Beta" },
+            { username: "26mda0076", password: "ESub807&8", nombre: "Usuario Beta" },
+            { username: "26mda0077", password: "ESub696[5", nombre: "Usuario Beta" },
+            { username: "26mda0078", password: "ESub909{9", nombre: "Usuario Beta" },
+            { username: "26mda0079", password: "ESub690$0", nombre: "Usuario Beta" },
+            { username: "26mda0080", password: "ESub367{1", nombre: "Usuario Beta" },
+            { username: "26mda0081", password: "ESub995%8", nombre: "Usuario Beta" },
+            { username: "26mda0082", password: "ESub708)8", nombre: "Usuario Beta" },
+            { username: "26mda0083", password: "ESub851^8", nombre: "Usuario Beta" },
+            { username: "26mda0084", password: "ESub745%5", nombre: "Usuario Beta" },
+            { username: "26mda0085", password: "ESub335[8", nombre: "Usuario Beta" },
+            { username: "26mda0086", password: "ESub550*0", nombre: "Usuario Beta" },
+            { username: "26mda0087", password: "ESub633<3", nombre: "Usuario Beta" },
+            { username: "26mda0088", password: "ESub821+5", nombre: "Usuario Beta" },
+            { username: "26mda0089", password: "ESub851%3", nombre: "Usuario Beta" },
+            { username: "26mda0090", password: "ESub578+4", nombre: "Usuario Beta" },
+            { username: "26mda0091", password: "ESub293(0", nombre: "Usuario Beta" },
+            { username: "26mda0092", password: "ESub634)1", nombre: "Usuario Beta" },
+            { username: "26mda0093", password: "ESub730$3", nombre: "Usuario Beta" },
+            { username: "26mda0094", password: "ESub138}0", nombre: "Usuario Beta" },
+            { username: "26mda0095", password: "ESub913!3", nombre: "Usuario Beta" },
+            { username: "26mda0096", password: "ESub559}0", nombre: "Usuario Beta" },
+            { username: "26mda0097", password: "ESub920_1", nombre: "Usuario Beta" },
+            { username: "26mda0098", password: "ESub549_6", nombre: "Usuario Beta" },
+            { username: "26mda0099", password: "ESub704(5", nombre: "Usuario Beta" },
+            { username: "26mda0100", password: "ESub803#3", nombre: "Usuario Beta" },
+            { username: "26mda0101", password: "ESub486)4", nombre: "Usuario Beta" },
+            { username: "26mda0102", password: "ESub892&5", nombre: "Usuario Beta" },
+            { username: "26mda0103", password: "ESub225<7", nombre: "Usuario Beta" },
+            { username: "26mda0104", password: "ESub834(8", nombre: "Usuario Beta" },
+            { username: "26mda0105", password: "ESub545[7", nombre: "Usuario Beta" },
+            { username: "26mda0106", password: "ESub262(4", nombre: "Usuario Beta" },
+            { username: "26mda0107", password: "ESub351_2", nombre: "Usuario Beta" },
+            { username: "26mda0108", password: "ESub869_7", nombre: "Usuario Beta" },
+            { username: "26mda0109", password: "ESub718<6", nombre: "Usuario Beta" },
+            { username: "26mda0110", password: "ESub457*5", nombre: "Usuario Beta" },
+            { username: "26mda0111", password: "ESub880>8", nombre: "Usuario Beta" },
+            { username: "26mda0112", password: "ESub985^3", nombre: "Usuario Beta" },
+            { username: "26mda0113", password: "ESub699!0", nombre: "Usuario Beta" },
+            { username: "26mda0114", password: "ESub240^3", nombre: "Usuario Beta" },
+            { username: "26mda0115", password: "ESub373@5", nombre: "Usuario Beta" },
+            { username: "26mda0116", password: "ESub948$3", nombre: "Usuario Beta" },
+            { username: "26mda0117", password: "ESub116<3", nombre: "Usuario Beta" },
+            { username: "26mda0118", password: "ESub582*4", nombre: "Usuario Beta" },
+            { username: "26mda0119", password: "ESub328@1", nombre: "Usuario Beta" },
+            { username: "26mda0120", password: "ESub950#5", nombre: "Usuario Beta" },
+            { username: "26mda0121", password: "ESub776%8", nombre: "Usuario Beta" },
+            { username: "26mda0122", password: "ESub740!0", nombre: "Usuario Beta" },
+            { username: "26mda0123", password: "ESub380]2", nombre: "Usuario Beta" },
+            { username: "26mda0124", password: "ESub209?3", nombre: "Usuario Beta" },
+            { username: "26mda0125", password: "ESub666$1", nombre: "Usuario Beta" },
+            { username: "26mda0126", password: "ESub246?2", nombre: "Usuario Beta" },
+            { username: "26mda0127", password: "ESub261+9", nombre: "Usuario Beta" },
+            { username: "26mda0128", password: "ESub390_2", nombre: "Usuario Beta" },
+            { username: "26mda0129", password: "ESub755&4", nombre: "Usuario Beta" },
+            { username: "26mda0130", password: "ESub167[7", nombre: "Usuario Beta" },
+            { username: "admin", password: "BetaMaster", nombre: "Usuario Beta Admin" }
+        ];
+
+
+function validarLogin() {
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
+    const mensaje = document.getElementById('mensajeError');
+
+    const usuario = usuariosValidos.find(
+        u => u.username === username && u.password === password
+    );
+
+    if (usuario) {
+        // Mostrar mensaje de carga y retrasar 2 segundos la redirección
+        mensaje.style.color = "green";
+        mensaje.textContent = "Iniciando sesión...";
+        setTimeout(function() {
+            window.location.href = "cursos.html";
+        }, 3000);
+    } else {
+        mensaje.style.color = "red";
+        mensaje.textContent = "Usuario o contraseña incorrectos";
+    }
+    return false;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('login');
+    if (form) {
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            validarLogin();
+        });
+    }
+
+    // Botón para mostrar/ocultar contraseña
+    const togglePassword = document.getElementById('togglePassword');
+    const passwordInput = document.getElementById('password');
+    
+    if (togglePassword && passwordInput) {
+        togglePassword.addEventListener('click', function() {
+            const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+            passwordInput.setAttribute('type', type);
+            this.classList.toggle('fa-eye');
+            this.classList.toggle('fa-eye-slash');
+        });
+    }
+});
